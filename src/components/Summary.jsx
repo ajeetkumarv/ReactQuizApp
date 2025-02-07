88ed0a1c75d15796e210337e4b7e3156b00d83f4
ajeetkumarv <ajeetkumarv@outlook.com>
2025-02-07 20:57:25 +0100

    Calculating percentage and rendering it

--- src/components/Summary.jsx
+++ src/components/Summary.jsx
@@ -2,20 +2,33 @@ import quizComplete from '../assets/quiz-complete.png';
 import QUESTIONS from '../questions.js';
 
 export default function Summary({userAnswers}) {
+
+  const skippedAnswerCount = userAnswers
+      .filter(ans => ans === null)
+      .length;
+      
+  const correctAnswerCount = userAnswers
+      .filter((ans, index) => ans === QUESTIONS[index].answers[0])
+      .length;
+  
+  const skippedPercent = Math.round(skippedAnswerCount * 100 / userAnswers.length);
+  const correctPercent = Math.round(correctAnswerCount * 100 / userAnswers.length);
+  const wrongPercent = 100 - skippedPercent - correctPercent;
+
     return <div id="summary">
           <img src={quizComplete} alt="quiz complete" />
           <h2>Quiz Completed!</h2>
           <div id="summary-stats">
             <p>
-              <span className="number">10%</span>
+              <span className="number">{skippedPercent}%</span>
               <span className="text">Skipped</span>
             </p>
             <p>
-              <span className="number">10%</span>
+              <span className="number">{correctPercent}%</span>
               <span className="text">Correct</span>
             </p>
             <p>
-              <span className="number">10%</span>
+              <span className="number">{wrongPercent}%</span>
               <span className="text">Wrong</span>
             </p>
           </div>
